8eee09c84d82ed65119364afe47a7568237be14d
Robin Thor <slash.and.axl@hotmail.de>
2020-03-12 14:50:21 +0100

    Implemented working Icon-Converter

--- Launchbox_Shortcut/Launchbox_Shortcut.cs
+++ Launchbox_Shortcut/Launchbox_Shortcut.cs
@@ -1,6 +1,6 @@
 ﻿using Unbroken.LaunchBox.Plugins;
 using Unbroken.LaunchBox.Plugins.Data;
-using System.Drawing;
+using System.Diagnostics;
 using System;
 using System.IO;
 using IWshRuntimeLibrary;
@@ -71,10 +71,29 @@ namespace BB_Shortcut
                     {
                         string rom_input = selectedGame.Title;
                         string rom_output = rom_input.Replace(':', ' ');
-                        string icon_input = selectedGame.FrontImagePath;
-                        string icon_convert = icon_input.Remove(icon_input.Length - 4);
-                        string icon_output = icon_convert + ".ico";
+                        string icon_input_path = selectedGame.FrontImagePath;
+                        string icon_output_raw= icon_input_path.Remove(icon_input_path.Length - 4);
+                        string icon_output_path = icon_output_raw + ".ico";
 
+                        // Convert-Icon
+                        string arguments = string.Format(@"-define jpeg:size=200x200 " + "\"" + icon_input_path + "\"" + " -thumbnail  \"256x256>\" -background transparent -gravity center -extent 256x256 " + "\"" + icon_output_path + "\""); ;
+                        string converter_path = Path.Combine(@"I:\LaunchBox\Plugins\convert.exe");
+                        var convert = new Process();
+                        convert.StartInfo.FileName = converter_path;
+                        convert.StartInfo.Arguments = arguments;
+                        convert.Start();
+
+                        //-------------------Debug-Console--------------------
+                        //Process p = new Process();
+                        //ProcessStartInfo psi = new ProcessStartInfo();
+                        //psi.FileName = "CMD.EXE";
+                        //psi.Arguments = "/K " + converter_path + " " + arguments;
+                        //p.StartInfo = psi;
+                        //p.Start();
+                        //p.WaitForExit();
+
+
+                        //Create-Shortcut
                         WshShell wsh = new WshShell();
                         IWshShortcut shortcut = wsh.CreateShortcut(
                           Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\" + rom_output + ".lnk"
@@ -82,7 +101,7 @@ namespace BB_Shortcut
                         shortcut.TargetPath = launchBoxPath + "\\" + emulator.ApplicationPath;
                         shortcut.Arguments = emulator.CommandLine + emulatorPlatform.CommandLine + " \"" + launchBoxPath + selectedGame.ApplicationPath + "\"";
                         shortcut.WorkingDirectory = Path.GetDirectoryName(shortcut.TargetPath);
-                        shortcut.IconLocation = icon_output;
+                        shortcut.IconLocation = icon_output_path;
                         shortcut.Save();
                         break;
                     }
@@ -95,3 +114,6 @@ namespace BB_Shortcut
         }
     }
 }
+
+
+
